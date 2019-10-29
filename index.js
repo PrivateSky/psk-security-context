a924176ac5955309a2a40029a70ad9b4a6f6fd7f
skutner <sava.dumitru.daniel@gmail.com>
2019-10-29 14:45:53 +0200

    removed EncryptedSecretList

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const TestSecurityContext = require("./lib/TestSecurityContext");
 const EncryptedSecret = require("./lib/EncryptedSecret");
-const EncryptedSecretList = require("./lib/EncryptedSecretList");
-const AgentList = require("./lib/AgentList");
 const PSKSignature = require("./lib/PSKSignature");
 
 module.exports.createSecurityContext = (securityContextType) => {
@@ -16,13 +14,7 @@ module.exports.createEncryptedSecret = (serializedEncryptedSecret) => {
     return new EncryptedSecret(serializedEncryptedSecret);
 };
 
-module.exports.createEncryptedSecretList = (serializedEncryptedSecretList) => {
-    return new EncryptedSecretList(serializedEncryptedSecretList);
-};
 
-module.exports.createAgentList = (serializedAgentList) => {
-    return new AgentList(serializedAgentList);
-};
 
 module.exports.createPSKSignature = (serializedPSKSignature) => {
     return new PSKSignature(serializedPSKSignature);
